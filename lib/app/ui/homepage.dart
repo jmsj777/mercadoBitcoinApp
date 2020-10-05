9f3051847641acdf12892ae27387ae8fa99ee95a
ze@home <jmsj777@hotmail.com>
2020-10-04 21:17:19 -0300

    with upgraded visuals

--- lib/app/ui/homepage.dart
+++ lib/app/ui/homepage.dart
@@ -13,142 +13,139 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String _low = '0';   
-  String _high = '0';   
-  String _vol = '0';   
-  List _asks;
-  List _bids;
+  String _low = '0';
+  String _high = '0';
+  String _vol = '0';
+  List<Widget> _asks = [Text("Clique no Botão!")];
+  List<Widget> _bids = [Text("Clique no Botão!")];
   String _date = '0';
 
   String _dropDownValue = 'BTC';
 
   Future<void> _initAttribs() async {
-    final resTicker = await http.get("https://www.mercadobitcoin.net/api/$_dropDownValue/ticker");
-    final resOrderBook = await http.get("https://www.mercadobitcoin.net/api/$_dropDownValue/orderbook");
+    final resTicker = await http
+        .get("https://www.mercadobitcoin.net/api/$_dropDownValue/ticker");
+    final resOrderBook = await http
+        .get("https://www.mercadobitcoin.net/api/$_dropDownValue/orderbook");
 
     Map<String, dynamic> resTickerParsed = json.decode(resTicker.body);
     final dateString = resTickerParsed['ticker']['date'];
     final date = DateTime.fromMicrosecondsSinceEpoch(dateString * 1000000);
     // print(date);
     setState(() {
-      _high = resTickerParsed['ticker']['high'].substring(0,7);
-      _low = resTickerParsed['ticker']['low'].substring(0,7);
-      _vol = resTickerParsed['ticker']['vol'].substring(0,7);
-      
+      _high = resTickerParsed['ticker']['high'].substring(0, 7);
+      _low = resTickerParsed['ticker']['low'].substring(0, 7);
+      _vol = resTickerParsed['ticker']['vol'].substring(0, 7);
+
       _date = '${date.day}/${date.month}, at ${date.hour}:${date.minute}';
-       });
-      
+    });
+
     Map<String, dynamic> resOrderBookParsed = json.decode(resOrderBook.body);
 
     var resAsks = resOrderBookParsed['asks'];
     var resBids = resOrderBookParsed['bids'];
     var listArrayAsks = new List<Widget>();
     var listArrayBids = new List<Widget>();
-    
+
     for (var i = 0; i < 20; i++) {
-      listArrayAsks.add(new Text('${resAsks[i][0].toStringAsFixed(1)}  /  ${resAsks[i][1].toStringAsFixed(6)}'));
-      listArrayBids.add(new Text('${resBids[i][0].toStringAsFixed(1)}  /  ${resBids[i][1].toStringAsFixed(6)}'));
+      listArrayAsks.add(new Text(
+          '${resAsks[resAsks.length - 1 - i][0].toStringAsFixed(1)}  /  ${resAsks[resAsks.length - 1 - i][1].toStringAsFixed(6)}'));
+      listArrayBids.add(new Text(
+          '${resBids[i][0].toStringAsFixed(1)}  /  ${resBids[i][1].toStringAsFixed(6)}'));
     }
 
-    print(listArrayAsks.runtimeType);
-    print(listArrayAsks[0].runtimeType);
+    // print(listArrayAsks.runtimeType);
+    // print(listArrayAsks[0].runtimeType);
     setState(() {
       _asks = listArrayAsks;
       _bids = listArrayBids;
     });
-      
-    
+  }
+
+  @override
+  void initState() {
+    _initAttribs();
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    // _initAttribs();
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        actions: <Widget>[
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          title: Text(widget.title),
+          actions: <Widget>[
             DropdownButton<String>(
               value: _dropDownValue,
               icon: Icon(Icons.arrow_downward),
               iconSize: 24,
+              iconDisabledColor: Colors.black,
+              iconEnabledColor: Colors.white70,
               elevation: 16,
-              style: TextStyle(
-                color: Colors.deepPurple
-              ),
-              
+              style: TextStyle(color: Colors.white),
               onChanged: (String newValue) {
                 setState(() {
                   _dropDownValue = newValue;
                 });
+                _initAttribs();
               },
               items: <String>['BTC', 'BCH', 'ETH', 'LTC']
-                .map<DropdownMenuItem<String>>((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                })
-                .toList(),
+                  .map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
             ),
             Padding(padding: EdgeInsets.all(10)),
-        ],
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        mainAxisSize: MainAxisSize.max,
-        children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[   
-              Padding(padding: EdgeInsets.all(10)),       
-              Text(
-                'Mínimo nas últimas 24h: $_low',
-                style: Theme.of(context).textTheme.headline5,
-              ),
-              Text(
-                'Máximo nas últimas 24h: $_high',
-                style: Theme.of(context).textTheme.headline5,
-              ),
-              Text(
-                'Volume Negociado: $_vol',
-                style: Theme.of(context).textTheme.headline5,
-              ),
-              
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Column(
-                      children: <Widget>[
+          ],
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Padding(padding: EdgeInsets.all(10)),
+                Text(
+                  'Mínimo nas últimas 24h: $_low',
+                  style: Theme.of(context).textTheme.headline5,
+                ),
+                Padding(padding: EdgeInsets.all(10)),
+                Text(
+                  'Máximo nas últimas 24h: $_high',
+                  style: Theme.of(context).textTheme.headline5,
+                ),
+                Padding(padding: EdgeInsets.all(10)),
+                Text(
+                  'Volume Negociado: $_vol',
+                  style: Theme.of(context).textTheme.headline5,
+                ),
+                Padding(padding: EdgeInsets.all(20)),
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Column(children: <Widget>[
                         Text("Ordens de compra (val  /  qty): "),
-                        for(var item in _asks)
-                          item,
-                      ]
-                  ),
-                  Column(
-                      children: [
+                        for (var i = 0; i < _asks.length; i++) _asks[i]
+                      ]),
+                      Column(children: [
                         Text("Ordens de venda (val  /  qty): "),
-                        for(var i=0; i<_bids.length; i++)
-                          _bids[i],
-                      ]
-                  ),
-                ]
-              ),
-            ],
-          ),
-          
-          Text(
-            'Updated in: $_date',
-          )
-        ],
-      ),
-    
-      
-      
-      floatingActionButton: FloatingActionButton(
-        onPressed: _initAttribs,
-        tooltip: 'Increment',
-        child: Icon(Icons.search),
-      )
-    );
+                        for (var i = 0; i < _bids.length; i++) _bids[i],
+                      ]),
+                    ]),
+              ],
+            ),
+            Text(
+              'Atualizado em: $_date',
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _initAttribs,
+          tooltip: 'Increment',
+          child: Icon(Icons.refresh),
+        ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -6,17 +6,28 @@ void main() {
   runApp(MyApp());
 }
 
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Mercado Bitcoin App'),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+            primarySwatch: Colors.green,
+            scaffoldBackgroundColor: Colors.black54,
+            visualDensity: VisualDensity.adaptivePlatformDensity,
+            textTheme:
+                Theme.of(context).textTheme.apply(bodyColor: Colors.white70),
+            canvasColor: Colors.green),
+        home: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/watermark.png"),
+                fit: BoxFit.scaleDown,
+                colorFilter: new ColorFilter.mode(
+                    Colors.black.withOpacity(0.2), BlendMode.dstATop),
+                alignment: Alignment.bottomLeft),
+          ),
+          child: MyHomePage(title: 'Mercado Bitcoin App'),
+        ));
   }
 }
